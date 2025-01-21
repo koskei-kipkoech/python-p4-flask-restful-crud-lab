3a92ac477c6fffb272f3e049ea0edc2a052b5e39
koskei-kipkoech <patrickwayy@gmail.com>
2025-01-21 11:01:17 +0300

    updates app.py

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, jsonify, request, make_response
+from flask import Flask, jsonify, request, make_response, abort
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 
@@ -46,7 +46,24 @@ class PlantByID(Resource):
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
-
+    
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if not plant:
+            abort(404, description=f'Plant with ID {id} not found')
+        data = request.get_json()
+        if 'is_in_stock' in data:
+            plant.is_in_stock = data['is_in_stock']
+        db.session.commit()
+        return make_response(jsonify(plant.to_dict()),200)
+    
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if not plant:
+            abort(404, description=f"Plant with  ID {id} not foud")
+        db.session.delete(plant)
+        db.session.commit()
+        return make_response('Deleyted Successfully',204)
 
 api.add_resource(PlantByID, '/plants/<int:id>')
 
